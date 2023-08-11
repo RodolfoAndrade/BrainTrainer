4eeed135721734f650a71529c8a4317103e9cee0
RodolfoAndrade <rodolfoivo@hotmail.com>
2023-08-11 18:21:20 +0200

    storing progress of math trainer.

--- QtApp/MentalMath.cpp
+++ QtApp/MentalMath.cpp
@@ -2,7 +2,9 @@
 
 MentalMath::MentalMath(QObject *parent)
 	: QObject(parent)
-{}
+{
+	readProgress();
+}
 
 MentalMath::~MentalMath()
 {}
@@ -20,6 +22,7 @@ QString MentalMath::generateEquation()
 	equation += QString::number(n1);
 	equation += "x";
 	equation += QString::number(n2);
+	MentalMath::equation = equation;
 
 	return equation;
 }
@@ -27,11 +30,56 @@ QString MentalMath::generateEquation()
 bool MentalMath::checkAnswer(QString answer)
 {
 	int ans = n1 * n2;
+	bool flag;
 	if (QString::compare(answer, "", Qt::CaseInsensitive) == 0) {
 		return false;
 	}
 	else {
 		qDebug() << "Message from QML: " << answer.toInt() << ans;
-		return answer.toInt() == ans;
+		flag = answer.toInt() == ans;
+		if (flag) {
+			saveProgress(equation);
+		}
+		return flag;
+	}
+}
+
+void MentalMath::saveProgress(QString equation)
+{
+	ofstream newFile("math_progress.txt");
+	if (in.find(equation) == in.end()) {
+		in[equation] = 1;
+	}
+	else {
+		in[equation]++;
+	}
+
+	// Get an iterator pointing to the first element in the map
+	map<QString, int>::iterator it = in.begin();
+
+	// Iterate through the map and print the elements
+	while (newFile.is_open() && it != in.end())
+	{
+		newFile << it->first.toStdString() << ":" << it->second << endl;
+		++it;
+	}
+	newFile.close();
+}
+
+void MentalMath::readProgress()
+{
+	ifstream newFile("math_progress.txt");
+	string line, key, val;
+	if (newFile.is_open())
+	{
+		while (getline(newFile, line))
+		{
+			qDebug() << line << '\n';
+			key = line.substr(0, line.find(":"));
+			val = line.substr(line.find(":") + 1, line.size());
+			qDebug() << key << ":" << val << '\n';
+			in[QString::fromStdString(key)] = stoi(val);
+		}
+		newFile.close();
 	}
 }
--- QtApp/MentalMath.h
+++ QtApp/MentalMath.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 #include <string> 
+#include <iostream>
+#include <fstream>
+using namespace std;
 
 class MentalMath  : public QObject
 {
@@ -14,9 +17,15 @@ public:
 	int n1 = 0;
 	int n2 = 0;
 	QString op;
+	QString equation;
 
 	explicit MentalMath(QObject *parent = 0);
 	~MentalMath();
 	Q_INVOKABLE QString generateEquation();
 	Q_INVOKABLE bool checkAnswer(QString answer);
+private:
+	map<QString, int> in;
+
+	void saveProgress(QString equation);
+	void readProgress();
 };
